95cc234d68472e1e13c0d3a12b20d368e0ebdb9e
Jim00000 <good0121good@gmail.com>
2018-08-13 20:40:24 +0800

    Add cli testcase

---
+++ src/test/java/jimvzero/findimg/AppTest.java
@@ -0,0 +1,36 @@
+package jimvzero.findimg;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class AppTest {
+
+	private final static Logger log = LogManager.getLogger(AppTest.class);
+
+	@Test
+	public void testcase1() {
+		log.info("Simulate App usage : ");
+		log.info("$ java -jar " + "findimg-0.0.1-SNAPSHOT-jar-with-dependencies.jar"
+				+ "-i src/test/resources/testcases/parse-case-1.html" + "-o ${tmp}/imgs" + "-sn");
+		final String tempDir = System.getProperty("java.io.tmpdir");
+		final Path outputDirPath = Paths.get(tempDir, "imgs"); 
+		final String[] args = { "findimg-0.0.1-SNAPSHOT-jar-with-dependencies.jar", "-i",
+				"src/test/resources/testcases/parse-case-1.html", "-o", outputDirPath.toString(), "-sn" };
+		App.main(args);
+		// Remove images directory
+		try {
+			FileUtils.deleteDirectory(outputDirPath.toFile());
+			log.info("Remove directory " + outputDirPath);
+		} catch (IOException e) {
+			Assertions.fail(e);
+		}
+	}
+
+}
