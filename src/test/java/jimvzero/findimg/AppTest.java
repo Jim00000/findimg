46bd67013b4df5384bb87778c13fdd8f4e80561e
Jim00000 <good0121good@gmail.com>
2018-08-13 21:27:02 +0800

    Update AppTest

--- src/test/java/jimvzero/findimg/AppTest.java
+++ src/test/java/jimvzero/findimg/AppTest.java
@@ -10,6 +10,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 
 import jimvzero.findimg.download.DownloadManagerTest;
 
@@ -17,24 +18,30 @@ public class AppTest {
 
 	private final static Logger log = LogManager.getLogger(AppTest.class);
 
+	@DisplayName("Test on Linux Platform")
 	@Test
 	public void testcase1() {
-		log.info("Simulate App usage : ");
-		log.info("$ java -jar " + "findimg-0.0.1-SNAPSHOT-jar-with-dependencies.jar"
-				+ "-i src/test/resources/testcases/parse-case-1.html" + "-o ${tmp}/imgs" + "-sn");
-		final String tempDir = System.getProperty("java.io.tmpdir");
-		final Path outputDirPath = Paths.get(tempDir, "imgs");
-		final String srcpath = DownloadManagerTest.class.getClassLoader().getResource("testcases/parse-case-1.html").getFile();
-		final String[] args = { "-i", srcpath, "-o", outputDirPath.toString(), "-sn" };
+		if (SystemUtils.IS_OS_LINUX) {
+			log.info("Simulate App usage on Linux : ");
+			log.info("$ java -jar " + "findimg-0.0.1-SNAPSHOT-jar-with-dependencies.jar"
+					+ "-i src/test/resources/testcases/parse-case-1.html" + "-o ${tmp}/imgs" + "-sn");
+			final String tempDir = System.getProperty("java.io.tmpdir");
+			final Path outputDirPath = Paths.get(tempDir, "imgs");
+			final String srcpath = DownloadManagerTest.class.getClassLoader().getResource("testcases/parse-case-1.html")
+					.getFile();
+			final String[] args = { "-i", srcpath, "-o", outputDirPath.toString(), "-sn" };
 
-		App.main(args);
-		// Remove images directory
-		try {
-			FileUtils.deleteDirectory(outputDirPath.toFile());
-			log.info("Remove directory " + outputDirPath);
-		} catch (IOException e) {
-			Assertions.fail(e);
+			App.main(args);
+			// Remove images directory
+			try {
+				FileUtils.deleteDirectory(outputDirPath.toFile());
+				log.info("Remove directory " + outputDirPath);
+			} catch (IOException e) {
+				Assertions.fail(e);
+			}
 		}
 	}
+	
+	///TODO: Add Windows platform Application simulation 
 
 }
