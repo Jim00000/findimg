4483fdd09485176b05135cef4fd015782f965801
Jim00000 <good0121good@gmail.com>
2018-08-14 15:55:16 +0800

    Update testcases

--- src/test/java/jimvzero/findimg/regex/RegexPatternFileReaderTest.java
+++ src/test/java/jimvzero/findimg/regex/RegexPatternFileReaderTest.java
@@ -10,9 +10,9 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 
 public class RegexPatternFileReaderTest {
-	
+
 	private final static Logger log = LogManager.getLogger(RegexPatternFileReaderTest.class);
-	
+
 	@DisplayName("Test reading default regex pattern file")
 	@Test
 	public void testcase1() {
@@ -24,12 +24,13 @@ public class RegexPatternFileReaderTest {
 			Assertions.fail(e);
 		}
 	}
-	
+
 	@DisplayName("Test reading dummy regex pattern file (Filename : dummy, Type : String)")
 	@Test
 	public void testcase2() {
 		try {
-			final String filepath = RegexPatternFileReaderTest.class.getClassLoader().getResource("testcases/regex/dummy").getPath();
+			final String filepath = RegexPatternFileReaderTest.class.getClassLoader()
+					.getResource("testcases/regex/dummy").getPath();
 			RegexPatternFileReader reader = new RegexPatternFileReader(filepath);
 			reader.getPatterns();
 		} catch (IOException e) {
@@ -37,12 +38,13 @@ public class RegexPatternFileReaderTest {
 			Assertions.fail(e);
 		}
 	}
-	
+
 	@DisplayName("Test reading dummy regex pattern file (Filename : dummy, Type : File)")
 	@Test
 	public void testcase3() {
 		try {
-			final String filepath = RegexPatternFileReaderTest.class.getClassLoader().getResource("testcases/regex/dummy").getPath();
+			final String filepath = RegexPatternFileReaderTest.class.getClassLoader()
+					.getResource("testcases/regex/dummy").getPath();
 			RegexPatternFileReader reader = new RegexPatternFileReader(new File(filepath));
 			reader.getPatterns();
 		} catch (IOException e) {
@@ -50,5 +52,19 @@ public class RegexPatternFileReaderTest {
 			Assertions.fail(e);
 		}
 	}
-	
+
+	@DisplayName("Test reading non-existent file")
+	@Test
+	public void testcase4() {
+		try {
+			final String filepath = "testcases/regex/dummy-non-existent-only-for-testcases-usage";
+			RegexPatternFileReader reader = new RegexPatternFileReader(new File(filepath));
+			reader.getPatterns();
+		} catch (IOException e) {
+			return; // Success
+		}
+		
+		Assertions.fail("This testcase should not come here");
+	}
+
 }
